2bbdbcfb7078afe83c1f48bbc451f12c7aabce97
Daniel Martins <mail.danielmartins@gmail.com>
2018-06-22 21:48:03 -0300

    Formulario e feed criados

---
+++ client/Feed/Feed.js
@@ -0,0 +1,19 @@
+Template.Feed.events({
+	"submit form": function (evento, template) {
+		evento.preventDefault();
+		var textoDoFormulario = evento.target.texto.value;		
+		console.log(textoDoFormulario);
+		Posts.insert({
+			texto: textoDoFormulario
+		})
+
+		evento.target.texto.value = ""
+	}
+});
+
+Template.Feed.helpers({
+	posts: function() {
+		var PostsDaCollection = Posts.find().fetch();
+		return PostsDaCollection
+	}
+});
